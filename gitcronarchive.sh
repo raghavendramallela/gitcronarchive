41d69b18aa0006c0aa6e8b1c6c60ef88bbb95cbc
MrG <58185871+raghavendramallela@users.noreply.github.com>
2022-05-27 20:36:44 +0530

    Update gitcronarchive.sh

--- gitcronarchive.sh
+++ gitcronarchive.sh
@@ -9,7 +9,7 @@ $GITWORKDIR=""
 mkdir -p ~/tmp/gitlocalcronarchive ~/tmp/untargitlocalcronarchive
 
 ## remove previous archives
-# rm -rf ~/tmp/gitlocalcronarchive/*tgz
+# rm -rf ~/tmp/gitlocalcronarchive/*.tgz
 
 cd $GITWORKDIR
 
